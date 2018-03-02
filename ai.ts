50a7bdb65def7f482163665ae62c8ac0c96049de
Mihai Raducanu <raducanu@gmail.com>
2018-03-02 11:53:06 +0200

    working network

--- ai.ts
+++ ai.ts
@@ -1,22 +1,53 @@
-import { Architect } from 'synaptic'
+import { Architect, Layer } from 'synaptic'
+import * as fs from 'fs'
 
-const base = new Architect.Perceptron(9, 9, 5)
-const o1 = new Architect.Perceptron(5, 9, 1)
+const base = new Architect.Perceptron(10, 10, 10, 9)
 
-console.log(base.activate([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
-const aa = base.project(o1)
-aa.
-console.log(base.activate([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
-console.log(
-  o1.activate(base.activate([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
-)
+interface TrainingItem {
+  input: number[]
+  output: number[]
+  position: number
+}
 
-o1.propagate(0.1, [1])
-console.log(base.activate([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
-console.log(
-  o1.activate(base.activate([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
-)
+const trainingSet: TrainingItem[] = []
 
-export const getMove = (table: number[]): number => {
-  return 0
+export const getPosition = (table: number[]): number => {
+  const odds = base.activate(table)
+  const best = odds.reduce(
+    (result, probability, position) =>
+      table[position] === 0.5 && probability > result.probability ? { position, probability } : result,
+    { position: 0, probability: 0 }
+  )
+
+  trainingSet.push({ input: table.slice(0), output: odds, position: best.position })
+  return best.position
+}
+
+export const train = (outcome: number) => {
+  const localSet = trainingSet.map(item => ({
+    ...item,
+    output: item.output.map((probability, i) => {
+      if (i === item.position)
+        if (outcome === 0.5) return 0.5
+        else if (outcome === item.input[9]) return 1
+        else return 0
+      else if (item.input[i] !== 0.5) return 0
+      else return probability
+    }),
+  }))
+
+  localSet.forEach(item => {
+    // console.log(item)
+    base.activate(item.input)
+    base.propagate(0.01, item.output)
+  })
+
+  trainingSet.length = 0
+}
+
+export const save = () => {
+  fs.writeFile('./network.json', JSON.stringify(base.toJSON()), (err: Error) => {
+    if (err) return console.log(err)
+    console.log('The file was saved!')
+  })
 }
--- runner.ts
+++ runner.ts
@@ -1,42 +1,63 @@
 import * as ai from './ai'
 import * as human from './human'
 
-export default async function runner() {
-  const table: number[] = new Array(9).fill(0.5)
-  const iterator: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-  log(table)
+interface Player {
+  getPosition: (table: number[]) => number
+  train: (outcome: number) => void
+}
 
-  for (const i of iterator) {
-    const piece = i % 2
-    const move: number = ai.getMove(table)
-    if (table[move] === 0.5) table[move] = piece
-    else throw new Error('invalid move')
-    if (isDone(table)) throw new Error('winner is ' + piece)
-    log(table)
+export default async function runner() {
+  let table: number[] = new Array(10).fill(0.5)
+  const iterator: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  // log(table)
+
+  let turnSwitch = Math.round(Math.random())
+  let draws = 0
+
+  for (let n = 0; n <= 500000; n++) {
+    table = new Array(10).fill(0.5)
+    turnSwitch = (turnSwitch + 1) % 2
+
+    try {
+      for (const i of iterator) {
+        const piece = (i + turnSwitch) % 2
+        table[9] = piece
+        const position: number = ai.getPosition(table)
+
+        if (table[position] === 0.5) table[position] = piece
+        else throw new Error('invalid move')
+
+        // log(table)
+
+        if (isWon(table)) {
+          // console.log('winner is ' + piece)
+          ai.train(piece)
+          if (draws > 100) console.log(draws, 'draws')
+          draws = 0
+          throw new Error('won')
+        }
+      }
+      // console.log('draw')
+      ai.train(0.5)
+      draws++
+    } catch (e) {
+      if (e.message !== 'won') throw e
+    }
   }
 
-  console.log('draw')
+  console.log(draws, 'draws')
+  ai.save()
 }
 
 const log = (table: number[]): void => {
   console.log(table[0].toFixed(1), table[1].toFixed(1), table[2].toFixed(1))
   console.log(table[3].toFixed(1), table[4].toFixed(1), table[5].toFixed(1))
   console.log(table[6].toFixed(1), table[7].toFixed(1), table[8].toFixed(1))
+  console.log(' ')
 }
 
-const isDone = (table: number[]): boolean => {
-  const sets = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
+const isWon = (table: number[]): boolean => {
+  const sets = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-  return sets.some(
-    set => set.every(p => table[p] === 0) || set.every(p => table[p] === 1)
-  )
+  return sets.some(set => set.every(p => table[p] === 0) || set.every(p => table[p] === 1))
 }
