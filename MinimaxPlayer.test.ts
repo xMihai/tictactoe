1a1a27885d03ab7beddb0e11453c4fd4ff828d9e
Mihai Raducanu <raducanu@gmail.com>
2018-03-22 17:29:08 +0200

    test: update

--- MinimaxPlayer.test.ts
+++ MinimaxPlayer.test.ts
@@ -5,8 +5,9 @@ describe('MinimaxPlayer', () => {
   test('getResults', () => {
     expect(MinimaxPlayer.getMinimax([1, 0, 1, 1, 0, 1, 0, 1, 0.5], Piece.X)).toEqual({
       bestPosition: 8,
-      bestResult: 1,
+      bestResult: 0.5,
     })
+
     expect(MinimaxPlayer.getMinimax([0, 1, 0, 1, 1, 0, 1, 0, 0.5], Piece.X)).toEqual({
       bestPosition: 8,
       bestResult: 1,
@@ -16,9 +17,15 @@ describe('MinimaxPlayer', () => {
       bestPosition: 7,
       bestResult: 1,
     })
+
     expect(MinimaxPlayer.getMinimax([0, 1, 0, 1, 1, 0, 0.5, 0.5, 0.5], Piece.X)).toEqual({
       bestPosition: 8,
       bestResult: 1,
+    })
+
+    expect(MinimaxPlayer.getMinimax([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5], Piece.X)).toEqual({
+      bestPosition: 0,
+      bestResult: 0.5,
     })
   })
 })
